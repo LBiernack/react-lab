be2aa96c45c281ca0d457bb58dbdc984507a9978
Lukasz Biernacki <lbiernacki@student.agh.edu.pl>
2024-05-18 14:01:40 +0200

    Reactivity

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'lbiernacki@agh.edu.pl';
+    //let email = 'lbiernacki@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
 
     function handleChange(event) {
-            console.log(event.target.value);
+            //console.log(event.target.value);
+            setEmail(event.target.value);
         }
+
+    let message;
+
+    if (email.length < 10) {
+        message = <p>"Ale masz któtki mail"</p>;
+    } else if (email.length >= 10 && email.length <15) {
+        message = <p>"Tówj email jest w sam raz"</p>;
+    } else {
+        message = <p>"Tówj email jest stanowczo za długi"</p>;
+    }
 
   return (
     <div>
          <h1>System do zapisów na zajęcia</h1>
           <h2>Twój e-mail to {email}</h2>
-          <input type="text" onChange={handleChange}/>
+          {message}
+          <input type="text" value={email} onChange={handleChange}/>
     </div>
   );
 }
